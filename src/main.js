497808a9f7652c7ffc489159fa53fd14f60aeaba
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-08-03 12:29:59 -0500

    modified main

--- src/main.js
+++ src/main.js
@@ -1,26 +1,13 @@
-//see the path relative or absolute and other
-//const { relative } = require('path');
-/* const path = require('path'); //property's node.js
-const route = 'readmeTest.md';
-
-const demo = path.join(__dirname, route);
-console.log(demo) */
 const path = require('path');
 
-const relativePath = 'src\\testLinks\\archivo1.md'//.md cambia
+const relativePath = 'src\\testLinks'
 const absolutePath = 'E:\\Diana_Angelica\\LIM015\\LIM015-md-links\\src\\testLinks\\archivo1.md';
-let isAbsolute = path.isAbsolute(relativePath) //Valido si es absoluta True
-//console.log(isAbsolute)
+
+//let isAbsolute = path.isAbsolute(absolutePath) //Valido si es absoluta True
 //Verifica si la ruta es absoluta y si es relativa devuelve absoluta
 function validatePath(relativePath){
     return path.isAbsolute(relativePath) === true ? relativePath : path.resolve(relativePath)
 }
-console.log(validatePath(relativePath));
-console.log(validatePath(absolutePath));
-/* console.log(__filename) */
-/* const path = require('path'); //property's node.js
-const absolute = path.isAbsolute('qux/')
-console.log(absolute); */
 module.exports = () => {
     validatePath
 };
