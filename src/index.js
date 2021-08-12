c2207f2d971873aeae39342e0fb72165d34e47b7
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-08-12 00:49:01 -0500

    add index to SRC

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const test = require('./main');
+
+
+/* console.log(test.getPathAbsolute('12548')); */
+
+/* module.exports = () => {
+  // ...
+}; */
+/* const { getPathAbsolute,
+  validateRoute,
+  isFile,
+  isMd,
+  readDirectory,
+  getFilesMd,
+  readFile,
+  getLinks,
+  validateLink
+} = require('../src/main'); */
